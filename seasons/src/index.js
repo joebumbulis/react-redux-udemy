f50f179d4b22f5292d6a9b047bb193b5a04d2594
Joe Bumbulis <joebumbulis@gmail.com>
2019-01-28 13:58:13 -0600

    adds comp did mount method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err),
-    );
+  //not required by React, but can be called in a Class
+  //overwrites the React.Component constructor with the component's constructor
+  //including the props and state
+  constructor(props) {
+    //must call super(props) - EVERY time
+    super(props);
+    // after calling super(props) - initialize state
+    // set state object with reasonable data
+    // data will be the latitude of the current position
+    // anytime the value is going to be a number, use null
+    // this.state is now accessible anywhere inside the App Class
+    // must be assigned to state property so YES: this.state / NO: this.myState
+    this.state = { latitude: null, errorMessage: '' };
+  }
 
-    return <div>Latitude: </div>
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // to update state object, we called setState()!!!
+        this.setState({ latitude: position.coords.latitude});
+        //DO NOT:
+        //this.state.latitude = position.coords.latitude
+        //only EVER use setState to UDPATE state -> never direct assignment
+      },
+      err => {
+        this.setState({ errorMessage: err.message })
+      },
+    );
+  }
+
+  //React says we MUST define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>
+    }
+    // the 'else' state, so don't need an else statement but
+    // rather render this default state
+    return <div>Loading!</div>
   }
 }
 
