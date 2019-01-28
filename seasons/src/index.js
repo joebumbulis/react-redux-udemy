51eec3e7f9ef5d3329bea76d317832558c1e473a
Joe Bumbulis <joebumbulis@gmail.com>
2019-01-28 14:05:16 -0600

    refactors state init from constructor into state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,20 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  //not required by React, but can be called in a Class
-  //overwrites the React.Component constructor with the component's constructor
-  //including the props and state
-  constructor(props) {
-    //must call super(props) - EVERY time
-    super(props);
-    // after calling super(props) - initialize state
-    // set state object with reasonable data
-    // data will be the latitude of the current position
-    // anytime the value is going to be a number, use null
-    // this.state is now accessible anywhere inside the App Class
-    // must be assigned to state property so YES: this.state / NO: this.myState
-    this.state = { latitude: null, errorMessage: '' };
-  }
+  state = { latitude: null, errorMessage: ''};
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
