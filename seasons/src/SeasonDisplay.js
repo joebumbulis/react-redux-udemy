785f718c8f2b6e21726abc77f923ca287fde9397
Joe Bumbulis <joebumbulis@gmail.com>
2019-01-29 11:55:52 -0600

    adds css styles to component

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -24,10 +25,10 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon massive`} />
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon massive`} />
+      <i className={`${iconName} icon massive icon-right`} />
     </div>
   );
 }
--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Spinner = () => {
-  return <i class="spinner loading icon"></i>;
+  return <i className="spinner loading icon"></i>;
 }
 
 export default Spinner;
